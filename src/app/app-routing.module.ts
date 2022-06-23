05e77fa9228e49fa932f0f9d3329b8c2d71c1f18
EL MOUELHI <elmouelhi.achref@gmail.com>
2022-06-23 09:39:18 +0200

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { DetailsPersonneComponent } from './components/details-personne/details-personne.component';
 import { PersonneComponent } from './components/personne/personne.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'personne', component: PersonneComponent },
+  { path: 'personne', component: PersonneComponent, canActivate: [AuthGuard] },
   { path: 'auth', component: AuthComponent },
   { path: 'personne/:id', component: DetailsPersonneComponent }
 ];
